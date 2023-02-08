2f913ef6840aa3a471ce75f59aff7fa21cf1eb4e
Karan Khatri <karankhatri535@gmail.com>
2023-02-08 23:03:29 +0530

    about component done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Info from './components/Info';
+import About from './components/About';
 
 function App() {
   return (
     <div className='outer-box'>
       <div className='inner-box'>
         <Info />
+        <About />
       </div>
     </div>
   );
